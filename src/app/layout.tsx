913ce556ed7ed01df3ffcfd391cc0ee465087ebf
invalidcode232 <mail@invalidcode.me>
2025-06-08 08:23:14 +0800

    fix(layout): change main text color from gray to white for improved visibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`h-screen overflow-scroll py-10 2xl:px-72 2xl:pt-20 xl:px-20 xl:pt-16 lg:px-20 md:px-20 px-8 pt-14 flex flex-col bg-slate-900 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <main className="text-gray-200 h-full flex-1">{children}</main>
+        <main className="text-white h-full flex-1">{children}</main>
         <Footer />
       </body>
     </html>
